2a15303b50dd57ad4a194ed157ccaa44e702ceeb
CarlosJohanND <cjnavarrod@ufpso.edu.co>
2025-05-28 20:13:19 -0500

    cambios

--- Js/dashboardService.js
+++ Js/dashboardService.js
@@ -7,7 +7,7 @@ function logout(){
 
 function tokenValidate(){
     const TOKEN = localStorage.getItem('token')
-    if(TOKEN === 'QpwL5tke4Pnpja7x4'){
+    if(TOKEN === 'null'){
         location.href = '../index.html'
     }
     console.log('Autenticado ', TOKEN)   
--- Js/userService.js
+++ Js/userService.js
@@ -1,5 +1,5 @@
 function getUsers(page){
-    document.getElementById('cardHeader').innerHTML = '<h4>Listado de usuarios</h4>'
+    document.getElementById('cardHeader').innerHTML = '<h4>Listado de usuarios <i class="fa-solid fa-users"></h4>'
     
     fetch("https://reqres.in/api/users?page="+page, {
         method: "GET",
@@ -21,6 +21,7 @@ function getUsers(page){
     .then((response) =>{
         if(response.status === 200){
             let listusers = `
+                <button class="btn btn-outline-success" type="button" onclick="addUser()"><i class="fa-solid fa-user-plus"></i></button>
                 <table class="table table-striped table-bordered">
                 <thead>
                     <tr >
@@ -108,7 +109,7 @@ function showModalUser(user){
         <div class="modal-dialog modal-sm">
             <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Show User</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Show User <i class="fa-solid fa-user"></i></h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
@@ -134,5 +135,96 @@ function showModalUser(user){
         document.getElementById('modalUser')
     )
     modal.show()
+}
+function addUser(){
+     const modalUser = `
+        <!-- Modal -->
+        <div class="modal fade" id="modalUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="">Add User<i class="fa-solid fa-user-plus"></i></h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="card">
+                <div class="card-body">
+                    <form id"formAddUser">
+                        <div class="mb-3">
+                            <label for="first_name" class="form-label">First name</label>
+                            <input type="text" class="form-control" id="first_name" placeholder="First name input">
+                        </div>
+                        <div class="mb-3">
+                            <label for="last_name" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" id="last_name" placeholder="Last name input">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Email input">
+                        </div>
+                        <div class="mb-3">
+                            <label for="avatar" class="form-label">Avatar</label>
+                            <input type="url" class="form-control" id="avatar" placeholder="Avatar input">
+                        </div>
+                        <div class="mb-3 text-center">
+                            <button class="btn btn-success" type="button" onclick="saveUser()"><i class="fa-solid fa-floppy-disk"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+         </div>
+        </div>
+    `
+    document.getElementById('showModal').innerHTML = modalUser
+    const modal = new bootstrap.Modal(
+        document.getElementById('modalUser')
+    )
+    modal.show()
+}
+
+function saveUser(){
+    const form = document.getElementById('formAddUser')
+    if(form.checkValidity()){
+        const first_name = document.getElementById('first_name').value
+        const last_name = document.getElementById('last_name').value
+        const email = document.getElementById('email').value
+        const avatar = documet.getElementById('avatar').value
+        const userData = {first_name, last_name, email, avatar}
+        fetch("https://reqres.in/api/users"+page, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            'x-api-key': 'reqres-free-v1'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then((result) =>{
+        return result.json().then(
+            data => {
+                return {
+                    status: result.status,
+                    body: data
+                }
+            }
+        )
+        .then((response) => {
+            if(response.status === 201){
+                document.getElementById('info').innerHTML = '<h3>The user was save</h3>'
+            }else{
+                document.getElementById('info').innerHTML = '<h3>The usar wasnt save correctly</h3>'
+            }
+            const modalId = document.getElementById('modalUser')
+            const modal = bootstrap.Modal.getElementById(modalId)
+            modal.hide()
+        })
+    })
+    }else{
+        form.reportValidity()
+    }
 }
 //STARS ARE FORMING, BIG BANG STORMING; so leave a HOLE IN THE FIRMAMENT, leaving for a WORLD BEYOND, put HOLE IN THE SKY, leaving for a WORLD BEYOND
